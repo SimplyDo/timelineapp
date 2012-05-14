a8975a8d3318ce801e7698b9be8b9bce3011a6b3
John M. Carlin <thejohnny@gmail.com>
2012-05-14 15:41:24 -0500

    First pass at implementing modal timeline form

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,10 @@
 class TimelinesController < ApplicationController
   def index
+    @timeline = Timeline.new
     @timelines = Timeline.all
+  end
+
+  def create
+    Timeline.create(params[:timeline])
   end
 end
