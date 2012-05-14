72e28fab5f989a642764da9073af620a86468b56
John M. Carlin <thejohnny@gmail.com>
2012-05-14 12:43:00 -0500

    Moved mockup out of root project directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimelineApp::Application.config.secret_token = 'f9c99bdb2ffe86e58bab4c7d6e10462eacce8c478019cc2c88b6da4ae28c86b3f38bc59955e82e70b900f8cfe6fb176a9c80994f409a4e890c8ec7e8d9e5f408'
