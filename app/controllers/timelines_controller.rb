7b3428d7ea1e5a69c89e4f9e984bab44c7f95188
John M. Carlin <thejohnny@gmail.com>
2012-05-14 19:06:29 -0500

    Submit new timelines from modal form

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,10 +1,11 @@
 class TimelinesController < ApplicationController
   def index
     @timeline = Timeline.new
-    @timelines = Timeline.all
+    @timelines = Timeline.order("created_at DESC")
   end
 
   def create
     Timeline.create(params[:timeline])
+    redirect_to timelines_path
   end
 end
